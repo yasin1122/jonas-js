7957ad6474b44b3906451602bdf321d16b17a198
Yasin Cakal <whoisyasin@gmail.com>
2023-12-27 21:06:48 +0300

    data types

--- js-fund1/assignments.js
+++ js-fund1/assignments.js
@@ -3,5 +3,9 @@
 let country = 'USA'
 let continent = 'North America'
 let population = 300000000
-
 console.log(country, continent, population)
+
+// Data Types
+let isIsland = false
+let language
+console.log(typeof isIsland, typeof population, typeof country, typeof language)
