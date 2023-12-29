96bd26d30ee6788629b6d8340564f79b2e7fd782
Yasin Cakal <whoisyasin@gmail.com>
2023-12-29 21:13:40 +0300

    forecast printer

--- s1-js-fund1/assignments.js
+++ s1-js-fund1/assignments.js
@@ -79,3 +79,13 @@ const amplitudeCalc = arr => {
   return max - min
 }
 console.log(amplitudeCalc(temperatures))
+
+// Forecast printer
+const printForecast = arr => {
+  let forecast = '...'
+  for (let i = 0; i < arr.length; i++) {
+    forecast += ` ${arr[i]}C in ${i + 1} days ...`
+  }
+  console.log(forecast)
+}
+printForecast([17, 21, 23])
