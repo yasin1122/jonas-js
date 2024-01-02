4235a0882f6b1f8dd75c7ca7d17be0047810963c
Yasin Cakal <whoisyasin@gmail.com>
2024-01-02 08:32:04 +0300

    jan 2

--- s1-js-fundamentals-pt1.js
+++ s1-js-fundamentals-pt1.js
@@ -32,3 +32,5 @@ console.log('19' - '13' + '17') // 617
 console.log('19' - '13' + 17) // 23
 console.log('123' < 57) // false
 console.log(5 + 6 + '4' + 9 - 4 - 2) // 1143
+
+// Equality Operators: == vs. ===
